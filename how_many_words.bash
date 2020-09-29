44fd9937a6c95fd4a967abcf2458439f878f5816
Aydar <me@aydar.media>
2020-09-29 19:54:13 +0300

    Add files via upload

---
+++ how_many_words.bash
@@ -0,0 +1,13 @@
+text=$(pbpaste)
+if [ -z $text ]; then
+	echo '\033[32m\033[1mFound\033[0m nothing in the buffer'
+else
+	words=( $text )
+	num=${#words[@]}
+	if [[ $num > 1 ]]; then
+		ex=words
+	else
+		ex=word
+	fi
+	echo '\033[32m\033[1mCounted\033[0m total of \033[1m'${#words[@]}'\033[0m' $ex
+fi
